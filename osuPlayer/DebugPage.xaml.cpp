ce17e7417513cb2437543cc161fd8bdba0069a50
Norman Zhi <normanzyb@gmail.com>
2016-07-15 16:57:42 +0800

    Debug list now scrolls to bottom automatically

--- osuPlayer/DebugPage.xaml.cpp
+++ osuPlayer/DebugPage.xaml.cpp
@@ -19,6 +19,9 @@ using namespace Windows::UI::Xaml::Data;
 using namespace Windows::UI::Xaml::Input;
 using namespace Windows::UI::Xaml::Media;
 using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::UI::Xaml::Automation;
+using namespace Windows::UI::Xaml::Automation::Peers;
+using namespace Windows::UI::Xaml::Automation::Provider;
 
 DebugPage::DebugPage()
 {
@@ -35,5 +38,15 @@ void Debug::DebugPage::OnNavigatedTo(NavigationEventArgs ^ e)
 void Debug::DebugParams::Append(String ^ str)
 {
 	if (dispatcher)
-		dispatcher->RunAsync(CoreDispatcherPriority::Normal, ref new DispatchedHandler([=] { debugList->Items->Append(str); }));
+		dispatcher->RunAsync(CoreDispatcherPriority::Normal, ref new DispatchedHandler([=] {
+			debugList->Items->Append(str);
+			debugList->UpdateLayout();
+			//debugList->ScrollIntoView(str);
+
+			ListBoxAutomationPeer^ svAutomation = (ListBoxAutomationPeer^)ScrollViewerAutomationPeer::CreatePeerForElement(debugList);
+			IScrollProvider^ scrollInterface = (IScrollProvider^)svAutomation->GetPattern(PatternInterface::Scroll);
+			// If the vertical scroller is not available, the operation cannot be performed, which will raise an exception. 
+			if (scrollInterface->VerticallyScrollable)
+				scrollInterface->Scroll(ScrollAmount::NoAmount, ScrollAmount::LargeIncrement);
+		}));
 }
